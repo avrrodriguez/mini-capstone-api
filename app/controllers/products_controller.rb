b48b95ac87ecf8b9229a8d70aa9b75cdad15589b
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-08-31 15:31:40 -0400

    Adds create method with a post route for adding new products, adds http file to send requests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     # show one product
     render json: Product.find_by(id: params["id"]).as_json
   end
+
+  def create
+    product = Product.new(
+      name: "Eraser",
+      price: 1,
+      image_url: "https://www.quill.com/is/image/Quill/sp44335983_s7?$img400$",
+      description: "An eraser, used to erase pencil marks and others.",
+    )
+    product.save
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   get "/products", controller: "products", action: "index"
   get "/products/:id", controller: "products", action: "show"
+  post "/products", controller: "products", action: "create"
 end
