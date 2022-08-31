afa5f96c993e85900a155efe9e0d0cdfa9db82d7
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-08-31 12:31:42 -0400

    Adds 3 products to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Product.new(
+Product.create(
   name: "pencil",
   price: 2,
   image_url: "https://weareticonderoga.com/wp-content/uploads/2019/06/13308_GLAMOUR_2000X2000.png",
   description: "yellow pencil, type 2, Ticonderoga",
 )
 
-Product.new(
+Product.create(
   name: "ruler",
   price: 4,
   image_url: "https://cdn5.vectorstock.com/i/1000x1000/79/69/ruler-vector-6227969.jpg",
   description: "an orange ruler measuring up to 16 milimeteres",
 )
 
-Product.new(
+Product.create(
   name: "stapler",
   price: 5,
-  image_url: "",
-  description: "",
+  image_url: "https://bostitchoffice.com/media/catalog/product/cache/8d622783d0b606912c99e4ab1818c82c/b/4/b440-black_1_main_300dpi.jpg",
+  description: "black stapler",
 )
+
+puts "done"
