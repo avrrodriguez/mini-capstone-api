3ef940517b804fa3f4db832c7b60098816c11248
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-01 13:50:03 -0400

    Saves the product once the update action has been called

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,9 +23,11 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
 
     product.name = params["name"] || product.name
-    product.price = params["price"].to_i || product.price
+    product.price = params["price"] || product.price
     product.image_url = params["image_url"] || product.image_url
     product.description = params["description"] || product.description
+
+    product.save
 
     render json: product.as_json
   end
