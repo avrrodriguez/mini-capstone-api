3b17ce044dae4c7600ab6b526d671851a7db0784
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-02 15:15:10 -0400

    Adds product variables to index and show products jb files and changes controller to use template jb file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,37 +1,37 @@
 class ProductsController < ApplicationController
   def index
     # show all products
-    products = Product.all
-    render json: products.as_json(methods: [:total])
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
     # show one product
-    product = Product.find_by(id: params["id"])
-    render json: product.as_json(methods: [:is_discounted?, :tax, :total])
+    @product = Product.find_by(id: params["id"])
+    render template: "products/show"
   end
 
   def create
-    product = Product.create(
+    @product = Product.create(
       name: params["name"],
       price: params["price"].to_i,
       image_url: params["image_url"],
       description: params["description"],
     )
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def update
-    product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params["id"])
 
-    product.name = params["name"] || product.name
-    product.price = params["price"] || product.price
-    product.image_url = params["image_url"] || product.image_url
-    product.description = params["description"] || product.description
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
 
-    product.save
+    @product.save
 
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def destroy
