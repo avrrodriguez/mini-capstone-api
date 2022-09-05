665e6839d6795c91a2e1c97085780661743bd507
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-05 14:13:53 -0400

    changes

--- db/migrate/20220905175939_change_data_types.rb
+++ db/migrate/20220905175939_change_data_types.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypes < ActiveRecord::Migration[7.0]
   def change
     change_column :products, :description, :text
-    change_column :products, :price, :decimal, precision: 7, scale: 3
+    change_column :products, :price, :decimal, precision: 20, scale: 2
   end
 end
