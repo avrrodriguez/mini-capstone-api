7fa90c4c6b1b11c5e75e629a0ddeb27c0e06b7b8
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-06 16:01:26 -0400

    Adds more crud routes to suppliers

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,27 @@
 class SuppliersController < ApplicationController
+  def index
+    render json: Supplier.all.as_json
+  end
+
+  def show
+    supplier = Supplier.find(params[:id])
+    render json: supplier.as_json
+  end
+
+  def create
+    supplier = Supplier.new(
+      name: params["name"],
+      email: params["email"],
+      phone_number: params["phone_number"],
+    )
+    supplier.save
+    render json: supplier.as_json
+  end
+
+  def update
+    # yet to finish crud routes
+    supplier = Supplier.find(params[:id])
+    supplier.name = params["name"]
+    render json: { "message": "works" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  # product routes
   get "/products", controller: "products", action: "index"
   get "/products/:id", controller: "products", action: "show"
   post "/products", controller: "products", action: "create"
   patch "/products/:id", controller: "products", action: "update"
   delete "/products/:id", controller: "products", action: "destroy"
+
+  # supplier routes
+  get "/suppliers", controller: "suppliers", action: "index"
+  get "/suppliers/:id", controller: "suppliers", action: "show"
+  post "/suppliers", controller: "suppliers", action: "create"
+  patch "/suppliers/:id", controller: "suppliers", action: "update"
 end
