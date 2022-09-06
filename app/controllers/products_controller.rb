cc3b2a02bddaaa023e8759d58fe237dc926640c3
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-06 13:16:06 -0400

    Adding error management to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,13 +12,18 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.create(
+    @product = Product.new(
       name: params["name"],
       price: params["price"],
       image_url: params["image_url"],
       description: params["description"],
     )
-    render template: "products/show"
+
+    if @product.save
+      render template: "products/show"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -29,9 +34,11 @@ class ProductsController < ApplicationController
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
 
-    @product.save
-
-    render template: "products/show"
+    if @product.save
+      render template: "products/show"
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, length: { minimum: 10, maximum: 500 }
+
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, multiline: true
+
   def is_discounted?
     price < 10
   end
