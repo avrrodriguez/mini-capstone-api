1e48f7759d60076910a99bd0ab1be321bbf0bc99
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-12 13:30:08 -0400

    Adds has_many, through relationship between category model and product model to connect them

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   def is_discounted?
     price < 10
